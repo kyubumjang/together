ae7ccbe718197cf209f0c5f1dfc1d4065800b377
kyubumjang <jkb2221@gmail.com>
2024-03-27 14:07:43 +0000

    fix: delete unnecessary function

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,14 +1,5 @@
-const fetchGitHubUser = async () => {
+const refreshUser = async () => {
   // TODO: 수정 필요
-  const response = await fetch("https://api.github.com/user", {
-    headers: {
-      Authorization: `token ${localStorage.getItem("githubToken")}`,
-    },
-  });
-  if (!response.ok) {
-    throw new Error("Failed to fetch GitHub user");
-  }
-  return response.json();
 };
 
-export { fetchGitHubUser };
+export { refreshUser };
